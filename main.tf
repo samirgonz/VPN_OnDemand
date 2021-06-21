960394b1a375f89c83755bfa4b7ff389d7abdb96
samirgonz <samirnidal@gmail.com>
2021-06-21 13:32:42 -0400

    After adding Environment Cred.

--- main.tf
+++ main.tf
@@ -18,6 +18,6 @@ terraform {
 
 provider "aws" {
   region     = var.availability_zone_names
-  access_key = VPN_ACCESS_KEY
-  secret_key = VPN_SECRET_KEY
+  access_key = TF_VPN_ACCESS_KEY
+  secret_key = TF_VPN_SECRET_KEY
 }
